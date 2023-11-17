fc6a245c1504b7cb6ec7c54f582a899c2362ebed
Radhika shahade <radhikashahade0804@gmail.com>
2023-11-17 22:27:40 +0530

    Palindrome Number

---
+++ src/main/java/leetcodeproblems/easy/CheckPalindrome.java
@@ -0,0 +1,22 @@
+package src.main.java.leetcodeproblems.easy;
+// Problem:9 Palindrome Number
+public class CheckPalindrome {
+    public boolean isPalindrome(int x)
+    {
+        int num=x;
+        int rev =0;
+        while(x!=0)
+        {
+            int digit =x%10;
+            rev = rev*10+digit;
+            x = x/10;
+        }
+        return rev==num;
+
+    }
+
+    public static void main(String[] args) {
+        CheckPalindrome ck = new CheckPalindrome();
+        System.out.println(ck.isPalindrome(122)); ;
+    }
+}
