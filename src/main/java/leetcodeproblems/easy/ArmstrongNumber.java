03f5b3a4b9d62c157873b6f7120c28e7280c2fa9
Radhika shahade <radhikashahade0804@gmail.com>
2023-11-18 20:30:29 +0530

    Armstrong  Number

---
+++ src/main/java/leetcodeproblems/easy/ArmstrongNumber.java
@@ -0,0 +1,28 @@
+package leetcodeproblems.easy;
+//1134:  Armstrong number
+public class ArmstrongNumber {
+    public boolean isArmstrong(int x)
+    {
+        int originalNum= x;
+        int temp =x;
+        int count=0;
+        while(x!=0)
+        {
+            x/=10;
+            count++;
+        }
+      double sumOfPower=0;
+        while(temp!=0)
+        {
+           int digit= temp%10;
+           sumOfPower+= Math.pow(digit,count);
+           temp/=10;
+        }
+       return originalNum==sumOfPower;
+    }
+
+    public static void main(String[] args) {
+        ArmstrongNumber armstrongNumber= new ArmstrongNumber();
+        System.out.println(armstrongNumber.isArmstrong(170));
+    }
+}
