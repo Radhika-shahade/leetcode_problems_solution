452bbb27c1d8e19f3cd586396d864570f843ad51
Radhika shahade <radhikashahade0804@gmail.com>
2024-01-09 23:00:46 +0530

    practice

--- src/main/java/dsapractice/Fibonacci.java
+++ src/main/java/dsapractice/Fibonacci.java
@@ -1,16 +1,17 @@
 package dsapractice;
 
 public class Fibonacci {
-    public static void main(String[] args) {
+ public static void main(String [] args) {
         Fibonacci obj = new Fibonacci();
         obj.findFibonacciSeries(5);
     }
 
     //0, 1, 1, 2, 3, 5, 8,
-    public void findFibonacciSeries(int num)//1
+  public void findFibonacciSeries(int num)//1
     {
         int first = 0;
         int second = 1;
+
         int i = 0;
         while (i<num ) {
             System.out.println(+first);
@@ -21,4 +22,5 @@ public class Fibonacci {
             i++;
         }
     }
+
 }
