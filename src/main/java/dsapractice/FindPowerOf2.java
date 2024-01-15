21c704c3818ea078bc784630200f04208562be42
Radhika shahade <radhikashahade0804@gmail.com>
2024-01-15 22:24:22 +0530

    practice

--- src/main/java/dsapractice/FindPowerOf2.java
+++ src/main/java/dsapractice/FindPowerOf2.java
@@ -1,20 +1,30 @@
 package dsapractice;
 
-public class FindPowerOf2 {
+public class FindPowerOf2 extends Object{
     int power=2;
+
     public void findPowerUpToGivenNumber(int number) {
-        for (int i = 0; i <= number; i++) {
-            if (i == 0) {
-                System.out.println(1);
-            } else {
-                power = power * 2;
-                System.out.println(power);
+        try {
+            StringBuffer sb= new StringBuffer();
+
+            
+            for (int i = 0; i <= number; i++) {
+                if (i == 0) {
+                    System.out.println(1);
+                } else {
+                    power = power * 2;
+                    System.out.println(power);
+                }
             }
+        }
+        finally{
+
         }
     }
 
-    public static void main(String[] args) {
+    static public  void main(String[] ns) {
         FindPowerOf2 obj = new FindPowerOf2();
         obj.findPowerUpToGivenNumber(5);
     }
+
 }
