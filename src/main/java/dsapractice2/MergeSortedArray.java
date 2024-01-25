cbf4c349c6e5684cab8293bec40481a1a5a00bb5
Radhika shahade <radhikashahade0804@gmail.com>
2024-01-25 12:29:41 +0530

    practice

---
+++ src/main/java/dsapractice2/MergeSortedArray.java
@@ -0,0 +1,40 @@
+package dsapractice2;
+//Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+//        Output: [1,2,2,3,5,6]
+public class MergeSortedArray {
+
+
+    public void merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int p1= m-1;
+        int p2=n-1;
+        int i=m+n-1;
+        while(p2>=0)
+        {
+          if(nums2[p2]<nums1[p1]&&p1>=0)
+          {
+              nums1[i]=nums1[p1];
+              p1--;
+          }
+          else {
+              nums1[i]=nums2[p2];
+              p2--;
+          }
+          i--;
+        }
+        for(int p :nums1)
+        {
+            System.out.println(p);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        MergeSortedArray obj= new MergeSortedArray();
+        int nums1[] = {1};
+        int m = 1;
+        int [] nums2 = {};
+        int n = 0;
+        obj.merge(nums1,m,nums2,n);
+    }
+}
